75e64fe406ca6787d81a416f28966b158e6e9827
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-04-23 20:56:27 -0700

    added endpoints to serverjs

--- server.js
+++ server.js
@@ -202,12 +202,123 @@ let movies = [
 },
 ];
 
+//CREATE
+app.post('/users', (req,res) => {
+	const newUser = req.body;
+
+	if (newUser.name){
+	newUser.id = uuid.v4();
+	users.push(newUser);
+	res.status(201).json(newUser)
+	} else {
+	res.status(400).send('users need names')
+	}
+})
+
+//UPDATE
+app.put('/users/:id', (req,res) => {
+	const { id } = req.params;
+	const updatedUser = req.body;
+
+	let user = users.find(user => user.id == id);
+
+	if (user) {
+		user.name = updated.user.name;
+		res.status(200).json(user);								
+	} else {
+	res.status(400).send('no such user')
+  }
+})
+
+
+
 //READ
 app.get('/movies'), (req,res) => {
   res.status(200).json(movies);
-});
+})
 
 //READ
+app.get('.movies/:title', (req,res) => {
+	const { title } = req.params;
+	const movies = movies.find( movie => movie.Title === title );
+
+	if(movie) {
+	res.status(200).json(movie); 
+	} else {
+		res.status(400).send('no such movie')
+	}
+
+})
+
+//CREATE
+app.post('users/:id/:movieTitle', (req,res) => {
+	const { id, movieTitle } = req.params;
+
+	let user = users.find ( user => user.id == id );
+
+	if (user) {
+		user.favoriteMovies.push(movieTitle);
+		res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);;
+	} else {
+	res.status(400).send('no such user')
+	}
+})
+
+//DELETE
+app.delete('users/:id/:movieTitle', (req,res) => {
+	const { id, movieTitle } = req.params;
+
+	let user = users.find ( user => user.id == id );
+
+	if (user) {
+		user.favoriteMovies = user.favoriteMovies.filter (title => title !==movietitle);
+		res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);;
+	} else {
+	res.status(400).send('no such user')
+	}
+})
+
+//DELETE
+app.delete('users/:id', (req,res) => {
+	const { id } = req.params;
+
+	let user = users.find ( user => user.id == id );
+
+	if (user) {
+		users = users.filter (title => title != id);
+		res.status(200).send(`user ${id} has been deleted`);;
+	} else {
+	res.status(400).send('no such user')
+	}
+})
+
+//READ
+app.get('.movies/genre/:genreName', (req,res) => {
+	const { genreName } = req.params;
+	const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
+
+	if(genre) {
+	res.status(200).json(genre); 
+	} else {
+		res.status(400).send('no such genre')
+	}
+
+})
+
+//READ
+app.get('.movies/director/:directorName', (req,res) => {
+	const { directorName } = req.params;
+	const director = movies.find( movie => movie.director.name === directorName ).Director;
+
+	if(director) {
+	res.status(200).json(director); 
+	} else {
+		res.status(400).send('no such director')
+	}
+
+})
+
+
 
 
 app.listen(8080, () => console.log("listening on 8080"))
