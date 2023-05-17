02bd1ad606dcfaac22365e8258c469ebb8c64485
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-05-16 23:11:05 -0700

    push models.js to main

---
+++ models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
+});
+
+const userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectID, ref: 'Movie' }]
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
