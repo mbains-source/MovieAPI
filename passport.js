12fa498d1c1c7877e974c4b4a70e49c27bde5455
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-06-03 12:13:32 -0700

    passport.js fix indentations

--- passport.js
+++ passport.js
@@ -7,37 +7,44 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-passport.use(new LocalStrategy({
-  usernameField: 'Username',
-  passwordField: 'Password'
-}, (username, password, callback) => {
-  console.log(username + '  ' + password);
-  Users.findOne({ Username: username }, (error, user) => {
-    if (error) {
-      console.log(error);
-      return callback(error);
-    }
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "Username",
+      passwordField: "Password",
+    },
+    (username, password, callback) => {
+      console.log(username + " " + password);
+      Users.findOne({ Username: username })
+        .then((user) => {
+          if (!user) {
+            console.log("incorrect username");
+            return callback(null, false, {
+              message: "Incorrect username.",
+            });
+          }
+          if (!user.validatePassword(password)) {
+            console.log('incorrect password');
+            return callback(null, false, {message: 'Incorrect password.'});
+          }
 
-    if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username.'});
+          console.log("finished");
+          return callback(null, user);
+        })
+        
+        .catch((error) => {
+          console.log(error);
+          return callback(error, false);
+        });
     }
-
-    if (!user.validatePassword(password)) {
-      console.log('incorrect password');
-      return callback(null, false, {message: 'Incorrect password.'});
-    }
-
-    console.log('finished');
-    return callback(null, user);
-  });
-}));
+  )
+);
 
 passport.use(
-  new jwtStrategy(
+  new JWTStrategy(
     {
-      jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'your_jwt_secret'
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "my_secret_key",
     },
     (jwtPayload, callback) => {
       return Users.findById(jwtPayload._id)
