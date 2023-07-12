95fbc8ba34cc0350a804e02a44c17a05137591b2
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-07-12 13:37:39 -0700

    add netlify to allowed origins CORS

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const Genre = Models.Genre;
 let allowedOrigins = [
   "https://myflixmantajbains.herokuapp.com",
   "http://localhost:1234",
+  "myflixclientmantajbains.netlify.app"
 ];
 
 app.use(
